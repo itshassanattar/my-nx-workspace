0ab7d831b190eeb316fe5111e349949d96db6cbe
Hassan Attar <itshassanattar@gmail.com>
2025-11-10 09:51:38 -0800

    add hero component

--- apps/react-demo/vite.config.ts
+++ apps/react-demo/vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types='vitest' />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig(() => ({
   root: __dirname,
@@ -13,13 +14,13 @@ export default defineConfig(() => ({
     port: 4300,
     host: 'localhost',
   },
-  plugins: [react()],
+  plugins: [react(), nxViteTsPaths()],
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [ nxViteTsPaths() ],
   // },
   build: {
-    outDir: './dist',
+    outDir: './build',
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
